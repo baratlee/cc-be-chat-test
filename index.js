91a7a57bbef96cee6da2873dc0e854149c8478cd
baratlee <barat83@163.com>
2019-12-21 18:06:57 +0800

    commit source files

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const inquirer = require('inquirer');
+const WebSocket = require('ws');
+
+var wsclient = new WebSocket("ws://127.0.0.1:3000");
+
+wsclient.on('open', function open() {
+  // console.log('connected\n');
+});
+
+wsclient.on('close', function close() {
+  console.log('disconnected\n');
+});
+
+wsclient.on('message', function incoming(data) {
+  console.log(data+'\n');
+});
 
 const run = async () => {
-  const { name } = await askName();
-  while (true) {
-    const answers = await askChat();
-    const { message } = answers;
-    console.log(`${name}: `, message);
+  let name = "";
+  while ("" === name){
+    const { name } = await askName();
+    if ("" === name){
+      continue;
+    }
+    wsclient.send(name);
+    while (true) {
+      const answers = await askChat();
+      const { message } = answers;
+      if ("" === message){
+        continue;
+      }
+      wsclient.send(message);
+      // console.log(`${name}: `, message);
+    }
   }
 };
 
