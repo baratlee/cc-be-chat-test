cb55ce1647bb484388681af56d8863b098261e05
baratlee <barat83@163.com>
2019-12-24 21:27:16 +0800

    add comment

--- src/profanity_filter.js
+++ src/profanity_filter.js
@@ -1,13 +1,18 @@
-const fs = require("fs");
+const file_stream = require("fs");
 const CharTrie = require("./util/char_trie");
 
 class ProfanityFilter {
+
+    /**
+     * @param {string} file_name
+     * @return void
+     */
     constructor(file_name) {
         let ct = new CharTrie();
         this.head = ct;
-        this.init_finish = false;
+        this.init_finish = false;    // flag of init finish
         let inner_this = this;
-        fs.readFile(file_name, "utf-8", function (error, file_data) {
+        file_stream.readFile(file_name, "utf-8", function (error, file_data) {
             if (error) return console.log("fail:" + error.message);
             let array_words = Array.from(file_data.split("\n"));
             console.log("success " + array_words.length);
